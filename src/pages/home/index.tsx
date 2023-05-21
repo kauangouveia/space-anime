6c889d81712afea74b5ed451b74bdd2ddc99b418
kauan.gouveia <kauangouveia64@gmail.com>
2023-05-21 00:37:27 -0300

    chore: create content for banner in home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from "@/components/Container";
+
+export default function Home() {
+    return (
+        <Container>
+            <div className="h-[580px] bg-red-700 w-full flex justify-center items-center">
+                <h1>Image Content</h1>
+            </div>
+        </Container>
+
+    )
+}
