d1ddf75c661d0fd7d8a471915ded247baf3944bf
kauan.gouveia <kauangouveia64@gmail.com>
2023-05-21 01:01:41 -0300

    chore: create list component

---
+++ src/components/List/index.tsx
@@ -0,0 +1,9 @@
+import { Title } from "../Title";
+
+export function List() {
+    return (
+        <div className="w-[1300px] h-[580px] bg-fuchsia-500">
+            <Title title="RECOMMENDED"/>
+        </div>
+    )
+}
