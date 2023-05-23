5adf9752c278dc3f17bcac98e11381c1042e59e6
kauan.gouveia <kauangouveia64@gmail.com>
2023-05-22 21:22:39 -0300

    chore: create model type for card

---
+++ src/@types/cards.ts
@@ -0,0 +1,22 @@
+export type CardModel = {
+    id: string;
+    type: string
+    attributes: {
+        slug: string;
+        canonicalTitle: string;
+        description: string;
+        posterImage: {
+            tiny: string;
+            large: string;
+            small: string;
+            medium: string;
+            original: string;
+        };
+        coverImage: {
+            tiny: string;
+            large: string;
+            small: string;
+            original: string;
+        };
+    }
+}
