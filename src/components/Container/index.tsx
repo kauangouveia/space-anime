4915ef0782e84978348f853610cfbd28b3650b01
kauan.gouveia <kauangouveia64@gmail.com>
2023-05-22 21:21:28 -0300

    fix: correct style

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -4,7 +4,7 @@ type ContainerProps = {
 
 export function Container({ children }: ContainerProps) {
     return (
-        <main className="w-screen h-screen flex justify-center items-center flex-col overflow-y-scroll">
+        <main className="w-screen h-auto flex justify-center items-center flex-col gap-3">
             {children}
         </main>
     )
