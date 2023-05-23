dd0630f7bca3962cd98f3b7bf8eab927c44d4b98
kauan.gouveia <kauangouveia64@gmail.com>
2023-05-23 20:37:10 -0300

    chore: add context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import { GlobalContextProvider } from '@/context/anime-context'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <GlobalContextProvider>
+      <Component {...pageProps} />
+    </GlobalContextProvider>
+  )
 }
