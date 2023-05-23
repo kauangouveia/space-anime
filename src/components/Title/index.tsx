83e47e6e7527ce46ba102165fc81ede6156dce93
kauan.gouveia <kauangouveia64@gmail.com>
2023-05-22 21:08:23 -0300

    chore: create component for title

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -3,8 +3,8 @@ type TitleProps = {
 }
 export function Title({ title }: TitleProps) {
     return (
-        <div className="flex-1 p-2 flex justify-start items-center">
-            <div className="w-2 h-10 bg-sky-700 rounded-full mr-1"></div>
+        <div className="flex-1 p-2 flex w-full justify-start items-center">
+            <div className="w-2 h-10 bg-sky-700 rounded-full mr-4"></div>
             <h1 className="text-xl font-medium">{title}</h1>
         </div>
     )
