e8d4a2850f362d33ccdcf067394b1fd55f9f7a7e
kauan.gouveia <kauangouveia64@gmail.com>
2023-05-22 21:08:00 -0300

    chore: use banners and list

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,16 @@
+import { Banner } from "@/components/Banner/Index";
 import { Container } from "@/components/Container";
 import { List } from "@/components/List";
+import { TipOfDay } from "@/components/TipOfDay";
 
 export default function Home() {
     return (
         <Container>
-            <div className="h-[580px] bg-red-700 w-full flex justify-center items-center">
-                <h1>Image Content</h1>
-            </div>
-            <List />
+            <Banner />
+            <List genre="romance" title="ROMANCE ANIMES" />
+            <List genre="action" title="ACTION ANIMES" />
+            <TipOfDay />
+            <List genre="adventure" title="ADVENTURE ANIMES" />
         </Container>
-
     )
 }
