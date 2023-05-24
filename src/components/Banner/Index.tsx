b8f23b45c3a7804ab6d8b45fc4124a906c0e42e6
kauan.gouveia <kauangouveia64@gmail.com>
2023-05-23 21:24:13 -0300

    chore: add function for see more

--- src/components/Banner/Index.tsx
+++ src/components/Banner/Index.tsx
@@ -1,17 +1,20 @@
 import Image from "next/image";
 import { Button } from "../Button";
 import { useListAnimeByGenre } from "@/hooks/useListAnimeByGenre";
+import { useGlobalContext } from "@/context/anime-context";
+import Link from "next/link";
 
 export function Banner() {
     const { listGenre } = useListAnimeByGenre('action')
-
+    const { setChosenAnime } = useGlobalContext();
     return (
         <div className="h-[580px] w-full flex justify-center items-center">
             <div className="w-[1300px] h-[580px] flex flex-col items-center justify-between  absolute p-10">
                 <h1 className="text-6xl font-bold text-gray-950">{listGenre[0]?.attributes.canonicalTitle.toUpperCase()}</h1>
                 <div className=" flex gap-3">
-                    <Button text="See more" handle={() => console.log()} />
-                    <Button text="Add To my list" handle={() => console.log()} />
+                    <Link href='/details'>
+                        <Button text="See more" handle={() => setChosenAnime(listGenre[0])} />
+                    </Link>
                 </div>
             </div>
             <Image src={listGenre[0]?.attributes.coverImage.original} alt="image" className='w-full h-full object-cover' width={920} height={200} />
