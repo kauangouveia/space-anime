61be5e2d9727fd0ea7d6e20f6104bf5943510203
kauan.gouveia <kauangouveia64@gmail.com>
2023-05-23 21:23:33 -0300

    chore: add icons

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -3,50 +3,44 @@ import { CardModel } from '@/@types/cards'
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import Card from "../Card";
 import { scroll } from '@/utils';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router'
+import { useGlobalContext } from '@/context/anime-context';
+import { AiFillCaretLeft, AiFillCaretRight } from 'react-icons/ai';
 
 type CarouselProps = {
     data: CardModel[];
     idElement: string;
 }
-type IdProps = {
-    id: string
-}
 
 export function Carousel({ data, idElement }: CarouselProps) {
+    const { setChosenAnime } = useGlobalContext();
 
     const [list, setList] = useState<CardModel[]>([])
-
-
-    const saveItemInList = (item: CardModel) => {
-        const newItems = [...list, item];
-
-        setList(newItems)
-
-        localStorage.setItem('list', JSON.stringify(list))
-
-    }
 
     const checkList = (item: CardModel) => {
         const itemExist = list.find(itemList => itemList.id === item.id)
 
         if (itemExist) return
-        saveItemInList(item)
+        const newItems = [...list, item];
+
+        localStorage.setItem('myList', JSON.stringify(newItems))
+        setList(newItems)
     }
     return (
         <div className="relative">
             <div className="absolute right-0  mt[-2rem]">
                 <button onClick={() => scroll(idElement, "LEFT")} className="p-2 m-2 rounded-full bg-white">
-                    <FiChevronLeft />
+                    <AiFillCaretLeft color='black' size={15} />
                 </button>
-                <button onClick={() => { scroll(idElement, "RIGHT"), console.log(list) }} className="p-2 m-2 rounded-full bg-white">
-                    <FiChevronRight />
+                <button onClick={() => scroll(idElement, "RIGHT")} className="p-2 m-2 rounded-full bg-white">
+                    <AiFillCaretRight color='black' size={15} />
                 </button>
             </div>
             <div id={idElement} className="carousel p-6 flex items-center justify-start overflow-x-auto scroll-smooth scrollbar-hide">
                 {data.map((item) => (
                     <div key={item.id}>
-                        <Card item={item} setNewItem={() => checkList(item)} />
+                        <Card item={item} setNewItem={() => checkList(item)} setChosenAnime={() => setChosenAnime(item)} />
                     </div>
                 ))}
             </div>
