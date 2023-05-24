412ab2cebe7a7f34e6f79247140cac418f0c4b09
kauan.gouveia <kauangouveia64@gmail.com>
2023-05-23 21:22:15 -0300

    chore: component my list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Banner } from "@/components/Banner/Index";
 import { Container } from "@/components/Container";
 import { List } from "@/components/List";
+import { MyList } from "@/components/MyList";
 import { TipOfDay } from "@/components/TipOfDay";
 
 
 export default function Home() {
+
   return (
     <Container>
       <Banner />
@@ -12,6 +14,7 @@ export default function Home() {
       <List genre="action" title="ACTION ANIMES" />
       <TipOfDay />
       <List genre="adventure" title="ADVENTURE ANIMES" />
+      <MyList />
     </Container>
   )
 }
